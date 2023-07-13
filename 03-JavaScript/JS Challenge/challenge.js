7e505e0f83faa0522ebc0dedb9418aa33363c069
NDenheim <nicole.denheim@hotmail.co.uk>
2023-07-13 15:15:28 +0100

    Completed challenge 2

--- 03-JavaScript/JS Challenge/challenge.js
+++ 03-JavaScript/JS Challenge/challenge.js
@@ -12,3 +12,13 @@ const isNumberPositive = (yourNumber) => {
 isNumberPositive(3);
 isNumberPositive(-3);
 isNumberPositive(18);
+
+// Write a function that takes a number of days and converts it into an age.
+
+const calculateAgeInYears = (ageInDays) => {
+    const ageInYears = ageInDays/365;
+    console.log(ageInYears);
+}
+
+calculateAgeInYears(3650);
+calculateAgeInYears(6570);
